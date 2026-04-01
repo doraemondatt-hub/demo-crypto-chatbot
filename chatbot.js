60c1d2b9d34462ec6e8fe8e10565ccc76e841784
Dat Tran <doraemondatt@gmail.com>
2026-04-02 00:53:57 +0700

    Testing new backend endpoint for email delivery

--- chatbot.js
+++ chatbot.js
@@ -260,7 +260,7 @@ window.addEventListener("DOMContentLoaded", initChatbot);
 // ============================================================
 
 // URL của Google Apps Script Web App
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxANW2XbaXYE0eYykmQQfMlASKj9YeKy-HyT78JNoF9iVG8UjFhA0Z6qmzk0_zPfOol8A/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxvXtanrhN3F9nqS7DdlkDmE6HleauVrsirKhADbqdufZ_z9F1Gachy7NE7nT-O_IJ30w/exec';
 
 // Tạo Session ID duy nhất cho mỗi phiên tải trang
 const AI_CHAT_SESSION_ID = 'session_' + Date.now() + '_' + Math.random().toString(36).substring(2, 7);
